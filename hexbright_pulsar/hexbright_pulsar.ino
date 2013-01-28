ac2effd9ef9b9be55eece4017b90068d81356aa2
Don Smith <don.c.smith@gmail.com>
2013-01-27 19:39:59 -0600

    new pulsar

--- hexbright_pulsar/hexbright_pulsar.ino
+++ hexbright_pulsar/hexbright_pulsar.ino
@@ -20,6 +20,15 @@
 #define MODE_OFF                0
 #define MODE_FADE               1
 #define MODE_ON                 2
+#define MODE_PULSAR             3
+
+// States
+#define STATE_OFF               0
+#define STATE_PULSE             1
+
+// Timing
+#define MAX_PULSE_MS          250;      
+#define GAMMA_DELTA            20;
 
 byte mode;
 boolean btnDown;
@@ -27,6 +36,22 @@ unsigned long lastTime, btnTime;
 int bright, fadeDir;
 
 byte delay_time = 20;
+
+int curChange;
+int changeDir;
+
+int startValue;
+int curValue;
+int endValue;
+int change;
+int waitTicks;
+int ticks;
+byte state;
+
+unsigned long time;
+boolean newBtnDown;
+
+
 
 void setup()
 {
@@ -46,84 +71,104 @@ void setup()
   Serial.begin(9600);
   Wire.begin();
   Serial.println("Ready.");
+  curChange = -20;
+  changeDir = 1;
   
   mode = MODE_OFF;
   btnDown = digitalRead(DPIN_RLED_SW);
   btnTime = millis();
 }
 
-void zoom(int first, int last, float delta) {
-  float diff = first - last;
-  float steps = round((first - last) / delta);
-  steps = abs(steps);
-  int bright = first;
+void lightOff() {
+  digitalWrite(DPIN_DRV_EN, LOW);
+  pinMode(DPIN_PWR,      OUTPUT);
+  digitalWrite(DPIN_PWR, LOW);
+}
 
-  for (float i = 0; i < steps; i++) {
-    unsigned int gamma = (bright*bright)>>8;
-    if (gamma < 6) gamma = 6;
-    Serial.print("gamma: ");
-    Serial.println(gamma);
-    
-    analogWrite(DPIN_DRV_EN, gamma);
-    bright += delta;
-  }  
+void lightOn(int val) {
+  pinMode(DPIN_PWR,      OUTPUT);
+  unsigned int gamma = (val*val)>>8;
+  analogWrite(DPIN_DRV_EN, gamma); 
+}
+
+
+void initPulse() {
+  startValue = 255;
+  curValue = startValue;
+  endValue = 0;
+  int span = abs(startValue - endValue);
+  int steps = span / GAMMA_DELTA;
+  change = MAX_PULSE_MS / steps;
+  change = 
+  waitTicks = 40;
+  ticks = 0;
+  state = STATE_PULSE;
+  Serial.println("Pulse started...");
 }
 
 void pulse() {
-  zoom(255, 0, -30);
-  delay(50);
-  zoom(255, 0, -30);
-  delay(50);
-  zoom(255, 0, -30);
-
-  delay(250);
+  unsigned long time = millis();
+  ticks++;
+  
+  
+  if (time-lastTime > 100 && newBtnDown) {
+    lastTime = time;
+    if (changeDir > 0 && curChange >= -5) {
+      changeDir = -2;
+    }
+    if (changeDir < 0 && curChange <= -30) {
+      changeDir = 2;
+    }
+    curChange += changeDir;
+    Serial.print(curChange);
+    Serial.print(" : ");
+    Serial.println(changeDir);
+  }
+  
+  switch (state) {
+    case STATE_PULSE:
+      lightOn(curValue);
+      
+      curValue += change;
+      
+      if ((change < 0 && curValue <= endValue) || (change > 0 && curValue >= endValue)) {
+        Serial.println("Pulse complete. Waiting...");
+        lightOff();
+        ticks = 0;
+        state = STATE_OFF;
+      }
+      break;
+    case STATE_OFF:
+      if (ticks >= waitTicks) {
+        Serial.print("Waiting complete.  ");
+        initPulse();
+      }
+      break;
+      
+  }
+  //delay(5);
 }
 
 void loop()
 {
-  unsigned long time = millis();
-  boolean newBtnDown = digitalRead(DPIN_RLED_SW);
-  
-  switch (mode)
-  {
-  case MODE_OFF:
-    if (btnDown && newBtnDown && time-btnTime>50)
-    {
-      mode = MODE_FADE;
-      bright = 0;
-      fadeDir = 10;
-      pinMode(DPIN_PWR,      OUTPUT);
-      digitalWrite(DPIN_PWR, HIGH);
-    }
-    break;
-  case MODE_FADE:
-    //if (time-lastTime > 5)
-//    if (1 == 1) {
-//      lastTime = time;
-//      if (fadeDir>0 && bright==255) fadeDir = -10;
-//      if (fadeDir<0 && bright==0  ) fadeDir =  10;
-//      bright += fadeDir;
-//      unsigned int gamma = (bright*bright)>>8;
-//      if (gamma < 6) gamma = 6;
-//      Serial.print("gamma: ");
-//      Serial.println(gamma);
-//      analogWrite(DPIN_DRV_EN, gamma);
-//    }
-    pulse();
-    if (!btnDown && !newBtnDown && time-btnTime>50)
-      mode = MODE_ON;
-    break;
-  case MODE_ON:
-    if (btnDown && newBtnDown && time-btnTime>200)
-      mode = MODE_FADE;
-    if (btnDown && !newBtnDown)
-    {
-      mode = MODE_OFF;
-      digitalWrite(DPIN_DRV_EN, LOW);
-      pinMode(DPIN_PWR,      OUTPUT);
-      digitalWrite(DPIN_PWR, LOW);
-    }
-    break;    
+  time = millis();
+  newBtnDown = digitalRead(DPIN_RLED_SW);
+  //////////
+  switch (mode) {
+    case MODE_OFF:
+      //if (btnDown && !newBtnDown && (time-btnTime)>20) {
+      if (newBtnDown) {
+        mode = MODE_PULSAR;
+        initPulse();
+      }
+      break;
+    case MODE_PULSAR:
+      pulse();
+      if (btnDown && !newBtnDown && (time-btnTime)>20) {
+        mode = MODE_OFF;
+        lightOff();
+      }
+      break;
   }
   
   if (newBtnDown != btnDown)
---
+++ pulsar_2/pulsar_2.ino
@@ -0,0 +1,267 @@
+/*
+  Hexbright demo firmware: Fades
+  Andrew Magill  9/2012
+  
+  1. Hold button down.  Light fades up and down.
+  2. Let go of button.  Light holds current brightness.
+  3. Press button.  Light turns off.
+*/
+
+#include <math.h>
+#include <Wire.h>
+
+// Pin assignments
+#define DPIN_RLED_SW            2
+#define DPIN_GLED               5
+#define DPIN_PWR                8
+#define DPIN_DRV_MODE           9
+#define DPIN_DRV_EN             10
+// Modes
+#define MODE_OFF                0
+//#define MODE_FADE               1
+//#define MODE_ON                 2
+#define MODE_LOW                1
+#define MODE_MED                2
+#define MODE_HIGH               3
+#define MODE_PULSAR             4
+
+
+// States
+#define STATE_OFF               0
+#define STATE_PULSE             1
+
+// Timing
+#define MAX_PULSE_MS         1000
+#define MIN_PULSE_MS          100
+
+// Button states
+#define BUTTON_DOWN             0
+#define BUTTON_UP               1
+
+
+byte btnState;
+
+byte mode;
+boolean btnDown;
+unsigned long lastTime, btnTime;
+int bright, fadeDir;
+
+byte delay_time = 20;
+
+int curChange;
+int changeDir;
+
+int startValue;
+int curValue;
+int endValue;
+int change;
+int waitTicks;
+int ticks;
+byte state;
+
+int holdLength;
+
+unsigned long stateBegan;
+unsigned long time;
+boolean newBtnDown;
+
+int pulseLength;
+int pulseChange = -50;
+
+void setup()
+{
+  pinMode(DPIN_PWR,      INPUT);
+  digitalWrite(DPIN_PWR, LOW);
+
+  // Initialize GPIO
+  pinMode(DPIN_RLED_SW,  INPUT);
+  pinMode(DPIN_GLED,     OUTPUT);
+  pinMode(DPIN_DRV_MODE, OUTPUT);
+  pinMode(DPIN_DRV_EN,   OUTPUT);
+  digitalWrite(DPIN_DRV_MODE, HIGH);
+  digitalWrite(DPIN_DRV_EN,   LOW);
+  digitalWrite(DPIN_GLED, HIGH);
+  
+  pulseLength = 250;
+  
+  btnState = BUTTON_UP;
+  
+  // Initialize serial busses
+  // Serial.begin(9600);
+  Wire.begin();
+  // Serial.println("Ready.");
+  curChange = -20;
+  changeDir = 1;
+  
+  mode = MODE_OFF;
+  btnDown = digitalRead(DPIN_RLED_SW);
+  btnTime = millis();
+}
+
+void lightOff() {
+  digitalWrite(DPIN_DRV_EN, LOW);
+  pinMode(DPIN_PWR,      OUTPUT);
+  digitalWrite(DPIN_PWR, LOW);
+}
+
+void lightOn(int val) {
+  pinMode(DPIN_PWR,      OUTPUT);
+  unsigned int gamma = (val*val)>>8;
+  analogWrite(DPIN_DRV_EN, gamma); 
+}
+
+
+void initPulse() {
+  state = STATE_PULSE;
+  lightOn(255);
+  stateBegan = millis();
+}
+
+
+void initWait() {
+  lightOff();
+  state = STATE_OFF;
+  stateBegan = millis(); 
+}
+
+int getPulseLength() {
+  return round(pulseLength / 2);
+}
+
+void pulse() {
+  time = millis();
+  int stateElap = time - stateBegan;
+  float diff = startValue - endValue;
+  float perComplete;
+  
+  if (time - lastTime > 100 && newBtnDown) {
+    lastTime = time;
+    if (pulseChange > 0 && pulseLength >= MAX_PULSE_MS) {
+      pulseChange = -50;
+    }
+    if (pulseChange < 0 && pulseLength <= MIN_PULSE_MS) {
+      pulseChange = 50;
+    }
+    pulseLength += pulseChange;
+    // Serial.print("New Pulse Length: ");
+    // Serial.println(pulseLength);
+  }
+  
+
+  
+  switch (state) {
+    case STATE_PULSE:
+      if (stateElap >= getPulseLength()) {
+        initWait();
+      }
+      break;
+    case STATE_OFF:
+       if (stateElap >= getPulseLength()) {
+        initPulse();
+      }
+      break;
+      
+  }
+}
+
+int lastHold() {
+  time = millis();
+  holdLength = time - btnTime;
+  return holdLength;
+}
+
+boolean clickLength(int sm, int lg) {
+  holdLength = lastHold();
+  return (holdLength >= sm && holdLength <= lg);
+} 
+
+int downFor() {
+  if (btnState == BUTTON_UP) {
+    return lastHold();
+  } else {
+    return 0; 
+  }
+}
+
+int upFor() {
+  if (btnState == BUTTON_DOWN) {
+    return lastHold();
+  } else {
+    return 0; 
+  }
+}
+
+
+void loop() {
+  time = millis();
+  newBtnDown = digitalRead(DPIN_RLED_SW);
+  
+
+  //////////
+  switch (mode) {
+    case MODE_OFF:
+      if (btnDown && !newBtnDown && clickLength(50, 10000)) {
+        mode = MODE_LOW;
+        lightOn(85); 
+        // Serial.println("Mode: LOW");
+      }
+      break;
+    case MODE_LOW:
+      if (btnDown && !newBtnDown && clickLength(50, 10000)) {
+        mode = MODE_MED;
+        lightOn(170);       
+        // Serial.println("Mode: MEDIUM");
+      }
+      break;
+    case MODE_MED:
+      if (btnDown && !newBtnDown && clickLength(50, 10000)) {
+        mode = MODE_HIGH;
+        lightOn(255);
+        // Serial.println("Mode: HIGH");
+      }
+      break;
+    case MODE_HIGH:
+
+      if (btnDown && !newBtnDown) {
+        if (clickLength(50, 250)) {
+          // Serial.println("Mode: PULSE");
+          mode = MODE_PULSAR;
+          initPulse();
+        } else {
+          // Serial.print("last: ");
+          // Serial.print(btnDown ? 'DOWN' : 'UP');
+          // Serial.print(", now: ");
+          // Serial.print(newBtnDown ? 'DOWN' : 'UP');
+          // Serial.print(", len: ");
+          // Serial.println(lastHold());
+        }
+      }
+      break;
+    case MODE_PULSAR:
+      pulse();
+      if (btnDown && !newBtnDown && clickLength(50, 250)) {
+        // Serial.println("Mode: OFF");
+        mode = MODE_OFF;
+        lightOff();
+      }
+      break;
+  }
+  
+  if (newBtnDown != btnDown) {
+    if (newBtnDown) {
+      btnState = BUTTON_DOWN;
+    } else {
+      btnState = BUTTON_UP;
+    }
+    
+    if (downFor() > 0) {
+      // Serial.print("Click Length: ");
+      // Serial.println(lastHold());
+    }
+    
+    btnTime = time;
+    btnDown = newBtnDown;
+    delay(50);
+  }
+}
+
